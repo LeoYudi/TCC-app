a813e370d90d0f71d53c01c63aae6a880669470a
Leonardo Higuti <lyhiguti@gmail.com>
2022-06-28 18:45:39 -0300

    add gps component

---
+++ src/components/Gps/index.js
@@ -0,0 +1,70 @@
+import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as GPS from 'expo-location';
+
+import { useRecord } from '../../contexts/Record';
+
+import { gpsConfig } from '../../config/gps';
+
+import style from './style';
+
+const round = number => number ? (Math.round(number * 100000) / 100000).toFixed(5) : 0;
+
+export default function Gps({ active }) {
+  const [subscription, setSubscription] = useState(null);
+  const [{ coords }, setLocation] = useState({});
+
+  const { isRecording, addRow } = useRecord();
+
+  const subscribe = async () => {
+    setSubscription(
+      await GPS.watchPositionAsync({
+        accuracy: GPS.Accuracy.Highest,
+        distanceInterval: 1,  // distancia mínima em metros para atualização
+        timeInterval: 30  // tempo mínimo em milisegundos para atualização
+      },
+        async location => {
+          setLocation(location);
+          await addRow(
+            gpsConfig.filename,
+            `${location.coords.latitude};${location.coords.longitude};${location.coords.altitude};${round(location.timestamp)}\n`
+          );
+        }
+      )
+    );
+  }
+
+  const unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await GPS.requestBackgroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.log(status);
+        return;
+      }
+
+      if (active && isRecording)
+        await subscribe();
+      else
+        unsubscribe();
+
+    })();
+
+    return () => unsubscribe();
+
+  }, [isRecording]);
+
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>GPS</Text>
+      <Text style={style.text}>lat: {coords?.latitude || 0}</Text>
+      <Text style={style.text}>lon: {coords?.longitude || 0}</Text>
+      <Text style={style.text}>alt: {round(coords?.altitude) || 0}</Text>
+    </View>
+  );
+}
---
+++ src/components/Gps/style.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+  container: {
+    width: '50%',
+    marginBottom: 30
+  },
+
+  text: {
+    color: 'white',
+    fontFamily: 'monospace',
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#fff'
+  },
+
+  title: {
+    color: '#DE9528',
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#fff',
+    marginBottom: 20
+  },
+
+  subTitle: {
+    textAlign: 'center',
+    fontSize: 10,
+    color: '#fff',
+    marginBottom: 5
+  }
+
+});
